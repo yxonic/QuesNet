1fd07a157c9fdcdb15ba082493787af10a5b0fa3
Yu Yin <yxonic@gmail.com>
2018-03-08 12:47:40 +0800

    simplify model example

--- app/models.py
+++ app/models.py
@@ -1,12 +1,7 @@
 class Simple:
     @staticmethod
     def add_arguments(parser):
-        parser.add_argument('-foo', default=10)
+        parser.add_argument('-foo', default=10, help='dumb param')
 
     def __init__(self, config):
         self.config = config
-
-
-class _Bar:
-    # Auxiliary classes starts with a _
-    pass
